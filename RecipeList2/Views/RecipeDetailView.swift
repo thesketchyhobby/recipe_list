8065bcb64bc782878fb0dd139ebb8c11c74f93c0
Heather Escoto <heather.escoto3@gmail.com>
2023-01-09 21:29:07 -0500

    Unable to type check adding equation to calculate recipe ingredients

--- RecipeList2/Views/RecipeDetailView.swift
+++ RecipeList2/Views/RecipeDetailView.swift
@@ -8,62 +8,76 @@
 import SwiftUI
 
 struct RecipeDetailView: View {
+    
     var recipe:Recipe
     
+    @State var selectedServingSize = 2
+    
     var body: some View {
-        ScrollView{
-            VStack(alignment: .leading){
-               
-                //MARK: Image
+        
+        ScrollView {
+        
+            VStack (alignment: .leading) {
+                
+                // MARK: Recipe Image
                 Image(recipe.image)
                     .resizable()
-                    .scaledToFit()
+                    .scaledToFill()
                 
-                //MARK: Ingredients
-                VStack(alignment:.leading){
-                        Text("Ingredients")
-                            .font(.headline)
-                            .padding([.bottom, .top], 5)
-                        
-                        ForEach (recipe.ingredients) {item in
-                            Text("*" + item.name)
-                }
-            }
-            .padding(.horizontal)
-            
-            Divider()
-                //MARK: Directions
-                VStack{
-                   
-                    VStack(alignment: .leading) {
-                        Text("Directions")
-                            .font(.headline)
-                            .padding([.bottom, .top], 5)
-                        
-                        ForEach(0..<recipe.directions.count, id: \.self) { index in
-                            
-                            Text(String(index+1) + ". " + recipe.directions[index])
-                                .padding(.bottom, 5)
-                        }
+                VStack (alignment: .leading) {
+                    Text("Select your serving size:")
+                    Picker("", selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
                     }
-                    .padding(.horizontal)
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width:160)
+                }
+                .padding()
                 
-        }
-        }
-            .navigationBarTitle(recipe.name)
-    }
-  }
-}
-                                                
+                // MARK: Ingredients
+                VStack(alignment: .leading) {
+                    Text("Ingredients")
+                        .font(.headline)
+                        .padding([.bottom, .top], 5)
+                    
+                    ForEach (recipe.ingredients) { item in
+                        Text("• " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name.lowercased())
+                    }
+                }
+                .padding(.horizontal)
+                
+                // MARK: Divider
+                Divider()
+                
+                // MARK: Directions
+                VStack(alignment: .leading) {
+                    Text("Directions")
+                        .font(.headline)
+                        .padding([.bottom, .top], 5)
+                    
+                    ForEach(0..<recipe.directions.count, id: \.self) { index in
+                        
+                        Text(String(index+1) + ". " + recipe.directions[index])
+                            .padding(.bottom, 5)
+                    }
+                }
+                .padding(.horizontal)
+            }
             
-        
-    
+        }
+        .navigationBarTitle(recipe.name)
+    }
+}
 
-
-struct RecipeDetailView_Previews:
-PreviewProvider {
+struct RecipeDetailView_Previews: PreviewProvider {
     static var previews: some View {
+        
+        // Create a dummy recipe and pass it into the detail view so that we can see a preview
         let model = RecipeModel()
+        
         RecipeDetailView(recipe: model.recipes[0])
     }
 }
